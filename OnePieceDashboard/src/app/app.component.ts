3cde0486de0b43d97f230cb8265b7faf368f994f
rajaramanrl <rajaram@runloyal.com>
2024-04-01 17:27:55 +0530

    Tabs Removed

--- OnePieceDashboard/src/app/app.component.ts
+++ OnePieceDashboard/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TabsComponent } from './Components/tabs/tabs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TabsComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
